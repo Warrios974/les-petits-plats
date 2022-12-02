a73b339a36bb757caed77195ea18dd72c9ac3de3
Warrios974 <kenny.lauret@outlook.fr>
2022-12-02 08:47:32 +0400

    Utilisation de l'objet Recette

--- scripts/components/cardReceipt.js
+++ scripts/components/cardReceipt.js
@@ -1,8 +1,11 @@
+
+import { receipt } from "../models/receipt.js";
+
 export function cardReceipt(data) {
 	//Défini chaque variable en fonction du contenu de "data"
-	const { id, name, servings, ingredients, time, description, appliance, ustensils } = data;
+	const objReceipt = receipt(data);
 
-	function createCardReceipt(receipt) {
+	function createCardReceipt() {
 		//Créer les differents éléments du DOM pour la création du media pour la galery
 		const article = document.createElement( "article" );
 		const divContainer = document.createElement( "div" );
@@ -20,19 +23,19 @@ export function cardReceipt(data) {
 		article.setAttribute("class","customCard col-4");
 		article.setAttribute("tabindex","0");
 		divContainer.setAttribute("class","container")
-		img.setAttribute("src","");
-		img.setAttribute("alt",name);
+		img.setAttribute("src",objReceipt.getImage());
+		img.setAttribute("alt",objReceipt.getName());
 		imgPart.setAttribute("class", "customCard__image");
 		descriptionPart.setAttribute("class","row justify-content-center customCard__description");
 		descriptionPartOne.setAttribute("class","row col-12");
 		descriptionPartTwo.setAttribute("class","row col-12");
-		titleh3.textContent = name;
+		titleh3.textContent = objReceipt.getName();
 		titleh3.setAttribute("class","col-8");
 		spanTime.setAttribute("class","col-4");
 		pIngredients.setAttribute("class","col-7");
 		pDescription.setAttribute("class","col-5");
-		spanTime.innerHTML = '<img src="./assets/medias/icons/clock.svg" alt=""> ' + time + ' min';
-		pDescription.textContent = description;
+		spanTime.innerHTML = '<img src="./assets/medias/icons/clock.svg" alt=""> ' + objReceipt.getTime() + ' min';
+		pDescription.textContent = objReceipt.getDescription();
 
 		article.appendChild(divContainer);
 		divContainer.appendChild(imgPart);
@@ -42,7 +45,7 @@ export function cardReceipt(data) {
 		descriptionPart.appendChild(descriptionPartOne);
 		descriptionPart.appendChild(descriptionPartTwo);
 
-		ingredients.forEach(ingredient => {
+		objReceipt.getIngredients().forEach(ingredient => {
 			const span = document.createElement( "span" );
 			span.innerHTML = '<strong>' + ingredient['ingredient'] + ' :</strong> ' + ingredient['quantity'] + ' ' + (ingredient['unit'] ? ingredient['unit'] : "") + '<br>';
 			pIngredients.appendChild(span);
@@ -55,5 +58,5 @@ export function cardReceipt(data) {
 		return (article);
 	}
 
-	return { id, name, servings, ingredients, time, description, appliance, ustensils, createCardReceipt};
+	return { createCardReceipt };
 }
